b94831a966cff879f9dbcd33c23c51c8ed641026
Liva <lclr@academy.cin.ufpe.br>
2023-02-21 00:55:19 -0300

    Initial Commit

---
+++ ChallengeSalADA/ChallengeSalADAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeSalADAApp.swift
+//  ChallengeSalADA
+//
+//  Created by livia on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeSalADAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
